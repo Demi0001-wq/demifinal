df578db8ee8d38f3d97bc5a5341fb52951e7ccea
Demi0001-wq <Demi0001-wq@users.noreply.github.com>
2026-02-23 21:31:55 +0530

    fix: Remove unused 'core.urls' reference to resolve final ModuleNotFoundError

--- 35.2/config/urls.py
+++ 35.2/config/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
     path('api/materials/', include('materials.urls')),
     path('api/users/', include('users.urls')),
     
